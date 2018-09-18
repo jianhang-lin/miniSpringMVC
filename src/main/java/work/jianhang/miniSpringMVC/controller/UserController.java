5a06c281fb3cccb8c391762ee18b5293d71ae678
jianhang-lin <ej2741@email.vccs.edu>
2018-09-18 11:23:52 +0800

    示例controller接口及具体实现

--- src/main/java/work/jianhang/miniSpringMVC/controller/UserController.java
+++ src/main/java/work/jianhang/miniSpringMVC/controller/UserController.java
@@ -1,4 +1,19 @@
 package work.jianhang.miniSpringMVC.controller;
 
+import work.jianhang.miniSpringMVC.Service.UserService;
+import work.jianhang.miniSpringMVC.annotation.Controller;
+import work.jianhang.miniSpringMVC.annotation.Qualifier;
+import work.jianhang.miniSpringMVC.annotation.RequestMapping;
+
+@Controller("userController")
+@RequestMapping("/user")
 public class UserController {
+
+    @Qualifier("userServiceImpl")
+    private UserService userService;
+
+    @RequestMapping("/insert")
+    public void insert() {
+        userService.insert();
+    }
 }
